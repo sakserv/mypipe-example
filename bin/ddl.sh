3c4650d29a32166161c319281ed7e4994256c9bd
Shane Kumpf <sakserv@sakserv.org>
2015-02-05 07:47:42 -0700

    added create table to ddl

--- bin/ddl.sh
+++ bin/ddl.sh
@@ -7,7 +7,16 @@ else
 fi
 MYSQL_USER=root
 MYSQL_PASS=horton
+MYSQL_DATABASE=mypipe
+MYSQL_TABLE=testing
 
 # Create the mypipe database
 echo -e "\n###  Creating the mypipe database"
-mysql --host "127.0.0.1" -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASS -Be "CREATE DATABASE mypipe;"
+mysql --host "127.0.0.1" -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASS -Be "CREATE DATABASE $MYSQL_DATABASE;"
+
+mysql --host "127.0.0.1" -P $MYSQL_PORT -u $MYSQL_USER -p$MYSQL_PASS -D $MYSQL_DATABASE -Be "CREATE TABLE $MYSQL_TABLE(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+firstname VARCHAR(30) NOT NULL, 
+lastname VARCHAR(30) NOT NULL, 
+score INT(3), 
+dt TIMESTAMP);"
