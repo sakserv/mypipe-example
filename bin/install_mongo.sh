6c772da75808c17eac975649d910ff18dbb0ec2b
Shane Kumpf <sakserv@sakserv.org>
2015-02-12 08:10:23 -0700

    remove kafka topic creation from mongo install

--- bin/install_mongo.sh
+++ bin/install_mongo.sh
@@ -17,7 +17,3 @@ cd /tmp/storm-topology-examples/
 cp bin/yum/mongodb.repo /etc/yum.repos.d/
 yum install mongodb-org -y
 service mongod start
-
-# Create the Kafka Topic
-echo -e "\n#### Creating the Kafka Topic: test_topic"
-/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --topic test_topic --zookeeper localhost:2181 --partitions 1 --replication-factor 1
