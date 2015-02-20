34edc90390015b45d448db875a2b6730561312ee
Shane Kumpf <sakserv@sakserv.org>
2015-02-20 16:09:10 -0700

    fix sed sub

--- bin/mypipe.sh
+++ bin/mypipe.sh
@@ -50,7 +50,7 @@ sed -e "s|@@DBHOST@@|$MYPIPE_DBHOST|g" \
     -e "s|@@DBPORT@@|$MYPIPE_DBPORT|g" \
     -e "s|@@DBUSER@@|$MYPIPE_DBUSER|g" \
     -e "s|@@DBPASSWORD@@|$MYPIPE_DBPASSWORD|g" \
-    -e "S|@@KAFKABROKERLIST@@|$KAFKA_BROKER_LIST|g" >$MYPIPE_APP_CONF_DEST
+    -e "s|@@KAFKABROKERLIST@@|$KAFKA_BROKER_LIST|g" >$MYPIPE_APP_CONF_DEST
 
 # Start mypipe
 echo -e "\n###  Starting mypipe"
