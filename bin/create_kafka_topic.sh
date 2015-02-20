a0ee1b298e985b50c8e65f0f232bacffd807cf9e
Shane Kumpf <sakserv@sakserv.org>
2015-02-20 15:51:26 -0700

    remove mongo stuff

--- bin/create_kafka_topic.sh
+++ bin/create_kafka_topic.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH=$PATH:/usr/hdp/current/kafka-broker/bin/
+
 if [ $# -ne 1 ]; then
   echo "ERROR: must supply the name of the topic you want to create"
   exit 1
@@ -8,4 +10,4 @@ topic="$1"
 
 # Create the Kafka Topic
 echo -e "\n#### Creating the Kafka Topic: $topc"
-/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --topic $topic --zookeeper localhost:2181 --partitions 1 --replication-factor 1
+kafka-topics.sh --create --topic $topic --zookeeper localhost:2181 --partitions 1 --replication-factor 1
